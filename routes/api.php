5ec2d0bfd79e6000555df77344a2f0c2c5c6aee5
salomeMumladze <salomemumladze775@gmail.com>
2025-09-16 13:55:15 +0400

    event management api

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee'=> 'event']);
+// scope means that this attendee and resources are always part of an event.
+// And basically this means that if you would use route model binding to get an attendee, Laravel will
+// automatically load it by looking only for the attendees of a parent event.
+// Now the routes for this controller will have both parameters, the event and attendee, 
+// and they will be both required.
